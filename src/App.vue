ebdd598241de59fd7aee6a74b14da67eced1db0d
Sonita09 <hengsonita33@gmail.com>
2025-01-16 08:19:47 +0700

    nita first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import DetailProductsView from './views/DetailProductsView.vue';
 </script>
 
 <template>
-
+  <DetailProductsView/>
   <RouterView />
 </template>
 
---
+++ src/views/DetailProductsView.vue
@@ -0,0 +1,201 @@
+<template>
+  <section>
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-4">
+          <div class="mb-3">
+            <img
+              class="w-100 h-100 object-fit-cover"
+              src="../assets/images/1.jpg"
+              alt=""
+            />
+          </div>
+          <div
+            class="d-flex justify-content-between align-content-center gap-3"
+          >
+            <div class="col-">
+              <img
+                class="w-100 h-100 object-fit-cover"
+                src="../assets/images/1.jpg"
+                alt=""
+              />
+            </div>
+            <div class="col-">
+              <img
+                class="w-100 h-100 object-fit-cover"
+                src="../assets/images/farm.jpg"
+                alt=""
+              />
+            </div>
+            <div class="col-">
+              <img
+                class="w-100 h-100 object-fit-cover"
+                src="../assets/images/myfarm.jpg"
+                alt=""
+              />
+            </div>
+            <div class="col-">
+              <img
+                class="w-100 h-100 object-fit-cover"
+                src="../assets/images/fish.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+        <div class="col-4 ps-3">
+          <div class="d-flex gap-3 align-content-center mb-3">
+            <div
+              class="d-flex justify-content-center gap-1 align-content-center"
+            >
+              <p class="mb-0 text-warning"><i class="bi bi-star-fill"></i></p>
+              <p class="mb-0 text-warning"><i class="bi bi-star-fill"></i></p>
+              <p class="mb-0 text-warning"><i class="bi bi-star-fill"></i></p>
+              <p class="mb-0 text-warning"><i class="bi bi-star-fill"></i></p>
+              <p class="mb-0 text-warning"><i class="bi bi-star-fill"></i></p>
+            </div>
+            <p class="mb-0 text-secondary-emphasis">99 Reviews</p>
+          </div>
+          <div>
+            <h4 class="mb-3">ផ្កាខាត់ណាស្រស់កំពត</h4>
+            <h1 class="primary-color fw-bold">
+              $40.00
+              <span
+                class="fw-bold fs-2 text-decoration-line-through support-color"
+                >$65.00</span
+              >
+            </h1>
+            <h5> : បន្លែ</h5>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="bg-white border-1 p-3 mb-3">
+            <div class="d-flex align-items-center mb-4">
+              <div class="d-flex align-items-center mb-2">
+                <div
+                  class="line-detail rounded-pill bg-primary-color me-2"
+                ></div>
+                <div
+                  class="dot-detail rounded-circle bg-primary-color me-3"
+                ></div>
+              </div>
+              <h5 class="">Top Rated Product</h5>
+            </div>
+            <div class="row px-3">
+              <div class="col-4 mb-3">
+                <img
+                  class="w-100 h-100 object-fit-cover"
+                  src="../assets/images/1.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="col-8 mb-3">
+                <div class="d-flex gap-1 align-content-center">
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                </div>
+                <p>Mixed solid Seat Cover</p>
+                <h6 class="primary-color fw-bold">
+                  $40.00
+                  <span
+                    class="fw-bold fs-6 text-decoration-line-through support-color"
+                    >$65.00</span
+                  >
+                </h6>
+              </div>
+              <div class="col-4 mb-3">
+                <img
+                  class="w-100 h-100 object-fit-cover"
+                  src="../assets/images/1.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="col-8 mb-3">
+                <div class="d-flex gap-1 align-content-center">
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                </div>
+                <p>Mixed solid Seat Cover</p>
+                <h6 class="primary-color fw-bold">
+                  $40.00
+                  <span
+                    class="fw-bold fs-6 text-decoration-line-through support-color"
+                    >$65.00</span
+                  >
+                </h6>
+              </div>
+              <div class="col-4 mb-3">
+                <img
+                  class="w-100 h-100 object-fit-cover"
+                  src="../assets/images/1.jpg"
+                  alt=""
+                />
+              </div>
+              <div class="col-8 mb-3">
+                <div class="d-flex gap-1 align-content-center">
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                  <p class="mb-0 text-warning">
+                    <i class="bi bi-star-fill"></i>
+                  </p>
+                </div>
+                <p>Mixed solid Seat Cover</p>
+                <h6 class="primary-color fw-bold">
+                  $40.00
+                  <span
+                    class="fw-bold fs-6 text-decoration-line-through support-color"
+                    >$65.00</span
+                  >
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div>
+              <img
+                src="../assets/images/posterDetail.gif"
+                class="w-100 h-100 object-fit-cover"
+                alt=""
+              />
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script setup></script>
