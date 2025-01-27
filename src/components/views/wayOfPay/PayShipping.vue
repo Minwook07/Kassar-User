e0135f6d375669fc1fa59dca2cce58110ac40d1e
GitSoriya <khloengsoriya@gmail.com>
2025-01-28 02:58:44 +0700

    process payment

---
+++ src/components/views/wayOfPay/PayShipping.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card-process py-5 mx-auto">
+        <h5 class="fw-bold">ដឹកជញ្ជូន</h5>
+        <small class="">កាលបរិច្ឆេទដឹកជញ្ជូន៖ 04-Sep-2025</small>
+
+        <hr class="hr">
+        <div class="d-flex justify-content-between mb-2">
+            <h6 class="fw-bold">តម្លៃ</h6>
+            <h6 class="fw-bold">80000 ៛</h6>
+        </div>
+        <div class="d-flex justify-content-between text-muted mb-2">
+            <span>បញ្ចុះតម្លៃ (5%)</span>
+            <span>-15000 ៛</span>
+        </div>
+        <div class="d-flex justify-content-between text-muted ">
+            <span>ថ្លៃដឹកជញ្ចូន</span>
+            <span>ឥតគិតថ្លៃ</span>
+        </div>
+        <hr class="hr">
+        <div class="d-flex justify-content-between fw-bold">
+            <span>តម្លៃសរុប</span>
+            <span>90000 ៛</span>
+        </div>
+        <hr class="hr">
+        <button class="mt-3 btn btn-secondary btn-process w-100" disabled>ទូទាត់
+            <span>៛90000</span></button>
+    </div>
+</template>
