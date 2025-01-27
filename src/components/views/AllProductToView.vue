e416c84804b26174bab668fe7a2b9088c46bac51
Sonita09 <hengsonita33@gmail.com>
2025-01-27 13:03:35 +0700

    all product

---
+++ src/components/views/AllProductToView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="bg-allProduct d-flex align-items-center justify-content-center">
+        <div class="">
+            <h1  class="text-secondary fw-bold">ទំនិញ</h1>
+        </div>
+    </div>
+</template>
+<script setup></script>
