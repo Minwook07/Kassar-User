5b2fa251b2a24802a4ddcb4c3f980e5163c9a5a6
Sonita09 <hengsonita33@gmail.com>
2025-01-28 23:42:26 +0700

    all product

--- src/components/views/AllProductToView.vue
+++ src/components/views/AllProductToView.vue
@@ -1,8 +1,222 @@
 <template>
+  <section style="background-color: #f2f2f2">
     <div class="bg-allProduct d-flex align-items-center justify-content-center">
-        <div class="">
-            <h1  class="text-secondary fw-bold">ទំនិញ</h1>
-        </div>
+      <div class="">
+        <h1 class="text-secondary fw-bold">ទំនិញ</h1>
+        <p class="text-center text-secondary">ទំព័រដើម / ទំនិញ</p>
+      </div>
     </div>
+    <div class="container-fluid my-5">
+      <div class="row">
+        <!-- <div class="col-2">
+          <h4 class="mb-0 fw-bold">ជម្រើសផលិតផល</h4>
+          <hr />
+          <div class="mb-3">
+            <h5 class="fw-bold mb-3">ប្រភេទ</h5>
+            <div class="mb-2 myform-check form-check">
+              <input
+                type="checkbox"
+                class="form-check-input shadow-none"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1">ត្រី</label>
+            </div>
+            <div class="mb-2 myform-check form-check">
+              <input
+                type="checkbox"
+                class="form-check-input shadow-none"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1">បន្លែ</label>
+            </div>
+            <div class="mb-2 myform-check form-check">
+              <input
+                type="checkbox"
+                class="form-check-input shadow-none"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1">ផ្លែឈើ</label>
+            </div>
+            <div class="mb-2 myform-check form-check">
+              <input
+                type="checkbox"
+                class="form-check-input shadow-none"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1">អង្ករ</label>
+            </div>
+            <div class="mb-2 myform-check form-check">
+              <input
+                type="checkbox"
+                class="form-check-input shadow-none"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1"
+                >អាហារគ្រៀម</label
+              >
+            </div>
+            <div class="mb-3 myform-check form-check">
+              <input
+                type="checkbox"
+                class="form-check-input shadow-none"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1"
+                >អាហារសម្រន់</label
+              >
+            </div>
+            <hr />
+          </div>
+          <div class="mb-3">
+            <h5>តម្លៃ</h5>
+            <div class="range-container">
+              <div class="double-slider-container"></div>
+            </div>
+            <div class="mb-3">
+              <h5 class="fw-bold mb-3">ម៉ាក</h5>
+              <div class="mb-2 myform-check form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input shadow-none"
+                  id="exampleCheck1"
+                />
+                <label class="form-check-label" for="exampleCheck1"
+                  >កសិដ្ឋានសិរីរាង្គ</label
+                >
+              </div>
+              <div class="mb-2 myform-check form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input shadow-none"
+                  id="exampleCheck1"
+                />
+                <label class="form-check-label" for="exampleCheck1"
+                  >Fresh Direct</label
+                >
+              </div>
+              <div class="mb-2 myform-check form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input shadow-none"
+                  id="exampleCheck1"
+                />
+                <label class="form-check-label" for="exampleCheck1"
+                  >Locak Harvest</label
+                >
+              </div>
+              <hr />
+            </div>
+            <div class="mb-3">
+              <h5 class="fw-bold mb-3">ស្តុក</h5>
+              <div class="mb-2 myform-check form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input shadow-none"
+                  id="exampleCheck1"
+                />
+                <label class="form-check-label" for="exampleCheck1"
+                  >មានក្នុងស្តុក</label
+                >
+              </div>
+              <div class="mb-2 myform-check form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input shadow-none"
+                  id="exampleCheck1"
+                />
+                <label class="form-check-label" for="exampleCheck1"
+                  >គ្មានក្នុងស្តុក</label
+                >
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div> -->
+        <div
+          class="col-12 col-md-4 col-lg-3 mb-3"
+          v-for="product in paginatedProducts"
+          :key="product.id"
+        >
+          <div
+            class="bg-white card card-product border-0 rounded position-relative"
+          >
+            <div class="card-img p-3">
+              <img
+                :src="product.img"
+                class="mycard-img-top rounded-top object-fit-cover"
+                alt=""
+              />
+            </div>
+            <div class="p-3 card-body">
+              <div class="d-flex justify-content-between">
+                <p class="text-primary mb-1">បន្លែ</p>
+                <p class="mb-1">
+                  <span class="text-warning me-2"
+                    ><i class="bi bi-star-fill"></i></span
+                  >4.9
+                </p>
+              </div>
+              <h4 class="fw-bold">{{ product.name }}</h4>
+              <p>500g</p>
+              <div class="d-flex justify-content-between align-items-center">
+                <p class="text-primary mb-0 fw-bold">
+                  10000៛
+                  <span class="text-decoration-line-through text-paragraph"
+                    >10000៛</span
+                  >
+                </p>
+                <router-link to="" class="btn btn-primary rounded-pill"
+                  ><i class="bi bi-bag-fill me-1"></i>កន្រ្តក</router-link
+                >
+              </div>
+            </div>
+            <div
+              class="position-absolute bg-primary card-product-discount top-0 ms-3 mt-3"
+            >
+              <p class="mb-0 px-3 text-white">20%</p>
+            </div>
+
+            <div
+              class="position-absolute border border-dark-subtle bg-white top-0 end-0 me-3 save-fav rounded-circle d-flex justify-content-center align-items-center"
+              @click="OnSavefav(product.id)"
+            >
+              <p class="mb-0 mt-1 text-danger fw-bold">
+                <i
+                  :class="allProduct.isFav ? 'bi bi-heart-fill' : 'bi bi-heart'"
+                ></i>
+              </p>
+            </div>
+          </div>
+        </div>
+        <paginate
+  :page-count="pageCount"
+  :click-handler="handlePageClick"
+  :prev-text="'Prev'"
+  :next-text="'Next'"
+  :container-class="'pagination'"
+/>
+      </div>
+    </div>
+  </section>
 </template>
-<script setup></script>
+<script setup>
+import { ref, computed } from "vue";
+import { useAllProducts } from "@/stores/views/allProduct_store";
+import Paginate from "vuejs-paginate-next";
+    const allProduct = useAllProducts();
+
+    const itemsPerPage = 2;
+    const currentPage = ref(1);
+    const pageCount = computed(() =>
+      Math.ceil(allProduct.products.length / itemsPerPage)
+    );
+    const paginatedProducts = computed(() => {
+      const start = (currentPage.value - 1) * itemsPerPage;
+      const end = start + itemsPerPage;
+      return allProduct.products.slice(start, end);
+    });
+    const handlePageClick = (pageNumber) => {
+      currentPage.value = pageNumber;
+    };
+    
+</script>
