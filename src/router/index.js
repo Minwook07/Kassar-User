34d2d678b5b87990ea714bc6361cb283a44e01a8
GitSoriya <khloengsoriya@gmail.com>
2025-03-18 20:57:37 +0700

    update animation and other page

--- src/router/index.js
+++ src/router/index.js
@@ -138,9 +138,9 @@ const router = createRouter({
       path: '/policy',
       name: 'policy',
       component: PolicyView,
-      path: '/termpage',
-      name: 'termpage',
-      component: TermPageView,
+      // path: '/termpage',
+      // name: 'termpage',
+      // component: TermPageView,
     },
     {
       path: '/privacy',
