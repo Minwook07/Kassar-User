99feccaecc6a33a397835d0c11824fe918fc0b27
Minwook07 <mith.chett@gmail.com>
2025-10-17 14:35:01 +0700

    add all shops pages

---
+++ src/stores/views/allShop_store.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useAllShopStore = defineStore('views/allshop', {
+    state: () => ({
+        shops: [],
+        loading: false,
+        error: null,
+        totalShop: 0,
+        pagination: {},
+    }),
+    getters: {
+        totalPages: (state) => state.pagination.last_page,
+        currentPage: (state) => state.pagination.current_page,
+    },
+    actions: {
+        async getShopLoad(page = 1, size = 15, scol = 'id', sdir = 'desc') {
+            this.loading = true
+            this.error = null
+            try {
+                const res = await axios.get(`http://kassar-api.test/api/shops?page=${page}&size=${size}&scol=${scol}&sdir=${sdir}`)
+                if (res.data.result) {
+                    this.shops = res.data.data
+                    this.pagination = res.data.paginate
+                }
+            } catch (err) {
+                this.error = err.message
+            } finally {
+                this.loading = false
+            }
+        },
+    },
+})
